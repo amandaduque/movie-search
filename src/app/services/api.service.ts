833c0d16812453818209e2ae7b0293c0f00c892f
Lydia <lydia.hillary@gmail.com>
2019-05-03 17:32:24 -0400

    Added styles and built out components. Added temporary favorite btn switch.
    Added styles and built out components. Added temporary favorite btn switch.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,9 +4,10 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class Api {
     apiURL = 'https://api.themoviedb.org/3/movie/top_rated?api_key=7213cee51893c8ccbb54b6ed9c49007b&language=en-US&page=1';
+    collURL = 'https://api.themoviedb.org/3/configuration?api_key=7213cee51893c8ccbb54b6ed9c49007b'
     constructor(private http: HttpClient) {}
 
     getMovies = () => this.http.get(this.apiURL);
+    getCollection = () => this.http.get(this.collURL);
     
 }
-
--- src/app/services/logger.service.ts
+++
@@ -1,5 +0,0 @@
-export class Logger {
-    info = (msg: string): void => console.info(`[${new Date()}]: ${msg}`);
-    error = (msg: string): void => console.error(`[${new Date()}]: ${msg}`);
-    warn = (msg:string): void => console.warn(`[${new Date()}]: ${msg}`);
-    }
