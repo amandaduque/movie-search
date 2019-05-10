14fe280a605c4be509462e70e429d5b63ef5feb2
Lydia <lydia.hillary@gmail.com>
2019-05-10 12:10:34 -0400

    Modified some things and finished up
    Got rid of "remove" option from initial and added "added" instead. Too unwieldy to have delete functionality from that component.
    
    Fixed styles on watchlist component. Added remove/delete functionality back in here.
    
    Fixed some general styles.

--- src/app/watchlistPage/watchlistPage.component.ts
+++ src/app/watchlistPage/watchlistPage.component.ts
@@ -1,6 +1,5 @@
 
 import { Component, Input, OnInit } from '@angular/core';
-import { movieListComponent } from '../movieList/movieList.component';
 import { Movie } from '../searchCriteria/searchCriteria.component';
 import { Api } from '../services/api.service';
 
@@ -9,13 +8,29 @@ import { Api } from '../services/api.service';
   templateUrl: './watchlistPage.component.html',
   styleUrls: ['./watchlistPage.component.css']
 })
+
 export class watchlistPageComponent implements OnInit {
   title: 'Watchlist';
   errorMessage: string;
   list: string;
-  watchList:any[]; 
+  watchList: Movie[]; 
+  urlFill: string = 'https://image.tmdb.org/t/p/w342';
+
   constructor(private api: Api) {}
+
   ngOnInit() {
     this.api.watchListArray.subscribe(list => this.watchList = list);
+  }
+
+  getUrl = (i) => {
+    if (this.watchList[i].poster_path === null) {
+      return '../assets/pic-filler.png';
+    } else {
+      return `${this.urlFill}${this.watchList[i].poster_path}`;
+    }
+  }
+
+  watchlistRemove = (i) => {
+    this.watchList.splice(i, 1);
   }
 }
